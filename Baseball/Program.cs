bda34379f27df293f3069955f59da1eff6cefd1e
Parkjungbok <sumeing.vogi@gmail.com>
2024-01-10 00:07:46 +0900

    feat : 컴퓨터 랜덤 배열 완성
    겹치지 않는 컴퓨터 4개의 숫자를 배열로 추출

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -21,19 +21,16 @@
             while (Com4 == Com1 || Com4 == Com2 || Com4 == Com3)
             { Com4 = Random(); }
 
-            int[] ComS = new int[4] 
-            { Com1, Com2, Com3, Com4 };
-
-            //Console.WriteLine($"{Com1}  {Com2}  {Com3}  {Com4}");
-
+            int[] ComS = new int[4] { Com1, Com2, Com3, Com4 };
+            
             return ComS;
         }
 
         static void Main(string[] args)
         {
-            ComSelect();
-            ComS[];
-            Console.WriteLine($"배열의 0번째 요소 : {ComS[0]}");
+            int[] ComS = ComSelect();
+
+            Console.WriteLine($"배열의 0번째 요소 : {ComS[0]},{ComS[1]},{ComS[2]},{ComS[3]}");
         }
     }
 }
