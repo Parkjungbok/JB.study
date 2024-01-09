c2e8ca5e16f336b6ea7dc8917fc5ec64e7b95a57
Jungbok <sumeing.vogi@gmail.com>
2024-01-09 17:18:19 +0900

    초기 커밋.

---
+++ Baseball/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Baseball
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ JB.study/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JB.study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
