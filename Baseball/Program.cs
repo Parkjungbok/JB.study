1fb4cef55a758fcbd9fae4c15ba1c76120920cf1
Jungbok <sumeing.vogi@gmail.com>
2024-01-09 17:53:43 +0900

    feat : 랜덤, 컴퓨터선택 추가
    랜덤함수 추가 및 컴퓨터가 각기 다른 4개의 숫자를 구하는 공식

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        public static int Random()
+        {
+            Random rand = new Random();
+            int Com = rand.Next(1, 9);
+            return Com;
+        }
+        public static void ComSelect()
+        {
+            int Com1 = Random();
+            int Com2 = Random();
+            while (Com2 == Com1)
+            { Com2 = Random(); }
+            int Com3 = Random();
+            while (Com3 == Com1 || Com3 == Com2)
+            { Com3 = Random(); }
+            int Com4 = Random();
+            while (Com4 == Com1 || Com4 == Com2 || Com4 == Com3)
+            { Com4 = Random(); }
+
+            Console.WriteLine($"{Com1}  {Com2}  {Com3}  {Com4}");
+
+            return;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ComSelect();
+            
         }
     }
 }
