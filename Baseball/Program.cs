eddb8d6951640f63730f595b9eb5057b49f8e18e
Parkjungbok <sumeing.vogi@gmail.com>
2024-01-15 01:29:49 +0900

    feat : 야구게임(진행중)
    슷자 입력, 입력된 숫자 배열 구성, 컴퓨터와 대조

--- Baseball/Program.cs
+++ Baseball/Program.cs
@@ -2,13 +2,13 @@
 {
     internal class Program
     {
-        public static int Random()
+        public static int Random()                                          // 랜덤
         {
             Random rand = new Random();
             int Com = rand.Next(1, 9);
             return Com;
         }
-        public static int[] ComSelect()
+        public static int[] ComSelect()                                     // 컴퓨터값 생성
         {
             int Com1 = Random();
             int Com2 = Random();
@@ -25,48 +25,93 @@
             
             return ComS;
         }
-        public static int Player()
+        public static int[] Player()                                        // 플레이어 입력
         {
             int number;
             do
             {
-                Console.Write("1~9 사이에 첫번째 숫자를 입력해주세요 : ");
+                Console.Write("1~9 사이에 네자리 숫자를 입력해주세요 : ");
                 string text = Console.ReadLine();
                 int.TryParse(text, out number);
-            } while (number < 1 || 9 < number);
-            
-            return number;
-        }
-        public static int PlayerSelect()
+            } while (number < 1111 || 9999 < number);
+
+            int[] playS = new int[4];
+            for (int i = 3; i >= 0; i--)
+            {
+                playS[i] = number % 10;
+                number /= 10;
+            }       
+
+            return playS;
+        }      
+        
+        public void Comparison()                                            // 두 수 비교
         {
-            int Player1 = Player();
-            //int Player2;
-            //int Player3;
-            //int Player4;
-            /*
-            Console.WriteLine("1~9 사이에 첫번째 숫자를 입력해주세요");            
-            int Player1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("1~9 사이에 두번째 숫자를 입력해주세요");
-            int Player2 = int.Parse(Console.ReadLine());
-            if (Player2 == Player1)
-            { }
+            int[] ComS = ComSelect();                                       // 컴퓨터
+            int[] player = Player();                                        // 플레이어
+
+            Console.WriteLine($"컴퓨터의 선택 : {ComS[0]},{ComS[1]},{ComS[2]},{ComS[3]}"); //체크용
+            Console.WriteLine($"플레이어의 선택 : {player[0]},{player[1]},{player[2]},{player[3]}");
+
+            int Strike = 0;
+            int Ball = 0;
+            int Out = 0;
+
+            if (ComS[0] == player[0])
+            { Strike += 1; }
+            else if (ComS[0] == player[1] || ComS[0] == player[2] || ComS[0] == player[3])
+            { Ball += 1; }
             else
-            { Console.WriteLine.("옳바르지 않습니다 다른 숫자를 입력해주세요."); }
-            */
+            { Out += 1; }
+
+            if (ComS[1] == player[1])
+            { Strike += 1; }
+            else if (ComS[1] == player[0] || ComS[1] == player[2] || ComS[1] == player[3])
+            { Ball += 1; }
+            else
+            { Out += 1; }
+
+            if (ComS[2] == player[2])
+            { Strike += 1; }
+            else if (ComS[2] == player[1] || ComS[0] == player[0] || ComS[0] == player[3])
+            { Ball += 1; }
+            else
+            { Out += 1; }
+
+            if (ComS[3] == player[3])
+            { Strike += 1; }
+            else if (ComS[3] == player[1] || ComS[3] == player[2] || ComS[3] == player[0])
+            { Ball += 1; }
+            else
+            { Out += 1; }
 
 
-            //int[] PS = new int[4] { Player1, Player2, Player3, Player4 };
+            Console.WriteLine($"라운드 결과는 {Strike}스트라이크, {Ball}볼, {Out}아웃 입니다.");
 
-            return Player1;
+            return 
+
         }
+
+        partial void Round()                                                // 라운드 반복
+        {
+            int[] ComS = ComSelect();                                       // 컴퓨터
+            //int[] player = Player();                                        // 플레이어
+
+            int Strike = 0;
+            int Ball = 0;
+            int Out = 0;
+
+            for (int i = 0; i = 10; i++)                                // 10라운드까지
+                {
+                    Comparison();
+                }
+        }
+
+        
         static void Main(string[] args)
         {
-            int[] ComS = ComSelect();
-            //int[] PS = PlayerSelect();
-            int player = Player();
-
-            Console.WriteLine($"컴퓨터의 선택 : {ComS[0]},{ComS[1]},{ComS[2]},{ComS[3]}");
-            Console.WriteLine($"플레이어의 선택 : {player}");
+            Console.WriteLine($"컴퓨터의 선택 : {ComS[0]},{ComS[1]},{ComS[2]},{ComS[3]}"); //체크용
+            Console.WriteLine($"플레이어의 선택 : {player[0]},{player[1]},{player[2]},{player[3]}");
         }
     }
 }
